785424a3f69550372c56710cf99f79ac589f22d1
kato-masafumi-lvgs <masafumi.kato@leverages.jp>
2019-12-02 20:34:29 +0900

    add console message

--- createTest_1.js
+++ createTest_1.js
@@ -3,6 +3,7 @@ const pages = require('./pages.json');
 
 const executeCapture = () => {
   Object.keys(pages).forEach(function (key) {
+    console.log(`creating image for ${pages[key]}`);
     capture(key, pages[key], 'tests');
   });
 }
